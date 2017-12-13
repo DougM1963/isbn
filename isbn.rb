f6e6376a0bd3152edb57456a8f9f87cef9c02035
dougm1963 <dougmeador1963@gmail.com>
2017-12-12 22:44:31 -0500

    still not working right

--- isbn.rb
+++ isbn.rb
@@ -1,20 +1,13 @@
-def isbn_count(isbn_number)
-    isbn_array = isbn_number.split("") { |s| s.to_i }
-    isbn_array.count
-end
+puts "Enter your ISBN number:"  # inputs Enter your ISBN number on screen
+isbn_number = gets.chomp  # assigns number entered to isbn_number
+isbn_array = isbn_number.split("").map(&:to_i) # numbers are displayed in an array as received in gets.chomp
+   print isbn_array  # prints the array
+   if isbn_number == 10 
+   puts "10 digits"
+   elsif isbn_number == 13
+   puts "13 digits"
+   else
+   puts "This is not a valid ISBN, it does not have 10 or 13 digits"
+   end
 
-def valid_isbn(isbn_number)
-    isbn_taken = isbn_number
-    if isbn_count(isbn_taken) == 10
-    puts "10 digits"
-    elsif isbn_count(isbn_taken) == 13
-    puts "13 digits"
-    else
-    puts "This is not a valid ISBN, it does not have 10 or 13 digits"
-    end
-end
-# Benchmark.bmbm do |results|
-        
-# end
 
-valid_isbn("12345678901231")
